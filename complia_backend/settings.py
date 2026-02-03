7c6a9e9f312b450a77eee8617124859e48de603f
Maaz Abdul Basith <maazabdulbasith@gmail.com>
2026-02-03 11:47:08 +0530

    Security: Rotate credentials and move to .env

--- complia_backend/settings.py
+++ complia_backend/settings.py
@@ -18,11 +18,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-76jm4k601elrh6by5x0wpg&kmzs_wao(g!u%&p)m08l-_l^woa'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
